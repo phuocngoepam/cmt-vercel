72360f4f8a2aff708b78a3c11268121dfcb5c9a6
Phuoc Ngo <cuibaps1@users.noreply.github.com>
2024-03-22 10:10:52 +0700

    init

---
+++ my-new-custom-application-project/custom-application-config.mjs
@@ -0,0 +1,39 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
+ */
+const config = {
+  name: 'Custom Product',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-au',
+  env: {
+    development: {
+      initialProjectKey: 'migration-test',
+    },
+    production: {
+      applicationId: 'clu0m8c4g000g5kdznv0kpyrq',
+      url: 'https://your_app_hostname.com',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
